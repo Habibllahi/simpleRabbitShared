aeb8fec231a01261823d4fece8ee92999555eb25
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2023-01-31 23:14:19 +0100

    update

--- src/main/java/com/codetrik/Message.java
+++ src/main/java/com/codetrik/Message.java
@@ -3,7 +3,9 @@ package com.codetrik;
 import com.codetrik.common.AMPQMessage;
 import com.rabbitmq.client.Channel;
 
+import java.io.IOException;
+
 public interface Message<T extends AMPQMessage> {
-    void publishMessage(Channel channel, T message);
-    void consumeMessage(Channel channel);
+    void publishMessage(Channel channel, T message) throws IOException;
+    T consumeMessage(Channel channel) throws IOException;
 }
